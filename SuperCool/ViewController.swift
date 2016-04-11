b9614b01cb174a96e9f760fb91a99fd7e95b44a3
Edmund Chang <eachang@Edmunds-iMac-2.local>
2016-04-11 11:40:57 -0700

    adding all files for the first time

--- SuperCool/ViewController.swift
+++ SuperCool/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var CoolLogo: UIImageView!
+    @IBOutlet weak var CoolBack: UIImageView!
+    @IBOutlet weak var UncoolButt: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +24,11 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func makemeNotUncool(sender: AnyObject) {
+        CoolLogo.hidden = false
+        CoolBack.hidden = false
+        UncoolButt.hidden = true
+    }
 
 }
 
